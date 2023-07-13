052406998e7ba783275e9d48eb1ae999b54fee06
nash.ocenar <nash.ocenar@ALGAR-WS-DEV004>
2023-07-13 17:12:24 +0800

    Add project files.

---
+++ taskui/Pages/ReleasePage.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace taskui.Pages
+{
+    public class ReleasePageModel : PageModel
+    {
+        private readonly ILogger<ReleasePageModel> _logger;
+
+        public ReleasePageModel(ILogger<ReleasePageModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
